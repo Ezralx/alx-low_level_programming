e2b26c6d5f29a941ebf3988ba6330153cb4a7928
Ezralx <ezrateshome.alx@gmail.com>
2023-03-20 15:10:42 -0700

    task 4:Print all the lowercase letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-Print all the lowercase letters except q and e
+ * Return: Alwyas 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
