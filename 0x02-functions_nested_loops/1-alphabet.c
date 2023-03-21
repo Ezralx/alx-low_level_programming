5c63fb6bf240040d7c78435d9b05a96d1dba2e99
Ezralx <ezrateshome.alx@gmail.com>
2023-03-21 15:13:52 -0700

    Task 1 of 0x02-functions_nested_loops:a programa function that prints the alphabet, in lowercase, followed by a new line that uses to only two  prints _putchar.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return:Always 0.
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+					
+		}
+}
+
