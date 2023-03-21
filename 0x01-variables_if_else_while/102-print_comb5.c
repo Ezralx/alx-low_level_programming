1786aca6fda9d7441ab959354d709c93dcea2471
Ezralx <ezrateshome.alx@gmail.com>
2023-03-20 17:42:54 -0700

    task 12:prints all possible combinations of two two-digit numbers the digits are ranging from 0 to 99  and separated  by comma  using putchar function

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,7 +13,7 @@ int main(void)
 	{
 		for (j = 0; j < 100; j++)
 		{
-			if (i > j)
+			if (i < j)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
@@ -26,6 +26,7 @@ int main(void)
 					putchar(' ');
 				}
 			}
+
 		}
 	}
 	putchar('\n');
