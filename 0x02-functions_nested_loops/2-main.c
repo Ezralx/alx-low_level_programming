5308bf6af580e54a05b79c52c34c539c43ad5d7a
Ezralx <ezrateshome.alx@gmail.com>
2023-03-22 11:54:16 -0700

    task 2 of 0x02-functions_nested_loopsa function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase, 
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints the alphabet
+ * in lowercase, followed by a new line
+ *
+ * Return:Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i <= 0; i++)
+	{
+		while (ch = 'a' && ch <= 'z')
+		{
+			_putchar(ch);
+		ch++;
+		}
+			_putchar('\n');
+	}
+}
+
