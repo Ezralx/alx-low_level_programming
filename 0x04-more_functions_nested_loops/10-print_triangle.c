59c19945a84799d2c03778d180843260eb03054f
Ezralx <ezrateshome.alx@gmail.com>
2023-03-25 17:33:35 -0700

    task 10 of 0x04-more functions_nested_loops:a function that prints a triangle, followed by a new line.Use the character # to print the triangle,size is the size of the triangle.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_triangle - a function that prints a triangle, followed by a new line.
+ * @size: size of the tirangle
+ * @#: character to print the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+
+			for (j = 1; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
+
+
