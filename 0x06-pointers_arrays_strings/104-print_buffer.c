8240b7f7e7f77f320b05c0a286162ee38c8da71e
Ezralx <ezrateshome.alx@gmail.com>
2023-03-31 15:43:40 -0700

    project 0x06-pointers_arrays_strings. Task 12.Noise is a buffer, more effective than cubicles or booth walls:a function that prints a buffer.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,40 +7,41 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, j, k;
+	int o, j, i;
 
-	i = 0;
+	o = 0;
+
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-	while (i < size)
+	while (o < size)
 	{
-		j = size - i < 10 ? size - i : 10;
-		printf("%.8x:", i);
-		for (k = 0; k < 10; k++)
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			if (k < j)
-				printf("%02x", *(b + i + k));
+			if (i < j)
+				printf("%02x", *(b + o + i));
 			else
-				printf(" ");
-			if (k % 2)
+				printf("  ");
+			if (i % 2)
 			{
 				printf(" ");
 			}
 		}
-		for (k = 0; k < j; k++)
+		for (i = 0; i < j; i++)
 		{
-			int c = *(b + i + k);
+			int c = *(b + o + i);
 
-			if (c < 32 || c  > 132)
+			if (c < 32 || c > 132)
 			{
 				c = '.';
 			}
 			printf("%c", c);
 		}
 		printf("\n");
-		i += 10;
+		o += 10;
 	}
 }
