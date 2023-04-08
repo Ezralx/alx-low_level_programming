d53583f0e1cf96b8ed0cf3b7f73740ec8494a6ed
Ezralx <ezrateshome.alx@gmail.com>
2023-04-08 13:03:03 -0700

    Project 0x0A.C-argc,argv:Task 0:a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,15 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s ", argv[i]);
-
-	}
+	printf("%s ", argv[argc * 0]);
 	printf("\n");
-	
 	return (0);
 }
 
