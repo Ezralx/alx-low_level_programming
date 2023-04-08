297f8795507eb26e048eae233b5bedeaa4b56368
Ezralx <ezrateshome.alx@gmail.com>
2023-04-08 15:21:45 -0700

    Project 0x0A.C-argc,argv:Task 3:a program that multiplies two numbers.Your program should print the result of the multiplication, followed by a new line;You can assume that the two numbers and result of the multiplication can be stored in an integer;If the program does not receive two arguments, your program should print Error, followed by a new line, and return 1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,13 @@
 int main(int argc, char *argv[])
 {
 	int i, j;
-       
+
 	if (argc == 3)
 	{
-		i = atoi(argv[1]);
-		j= atoi(argv[2]);
-		printf("%d\n", i * j);
-		return (0);
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	printf("%d\n", i * j);
+	return (0);
 	}
 	printf("Error\n");
 	return (1);
