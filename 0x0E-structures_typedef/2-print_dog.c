429d354ac31091ac285ebad31ea55d87fb0dad3f
Ezralx <ezrateshome.alx@gmail.com>
2023-04-18 15:28:51 -0700

    project:0x0E. C - Structures, typedef.Task 2:a function that prints a struct dog; If an element of d is NULL, print (nil) instead of this element. (if name is NULL, print Name: (nil)); If d is NULL print nothing.

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,10 +7,15 @@
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
+
 	{
 		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
 		printf("Name: %f\n", (d->age) ? d->age : 0);
 		printf("Name: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+	else
+	{
+		return;
 	}
 }
 
