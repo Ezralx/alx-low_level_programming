3208bb921af493adc735d3556479eadfc57c5ca4
Ezralx <ezrateshome.alx@gmail.com>
2023-04-21 17:51:43 -0700

    Project:0x0F. C - Function pointers; Task 1:a function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,13 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (size <= 0)
+	return (-1);
+	if (array == NULL || cmp == NULL)
+	return (-1);
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i] != 0))
-				return (i);
-		}
+	if (cmp(array[i]) == 1)
+	return (i);
 	}
 	return (-1);
 }
