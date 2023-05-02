07f59038d78ed0e8d220a0912baff3a06785102d
Ezralx <ezrateshome.alx@gmail.com>
2023-05-02 14:11:21 -0700

    Project:0x13-more_singly_linked_lists. Task 0: a function that prints all the elements of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: linked list pointer to the head
+ *
+ * Return:All the elments in the list(nodes)
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
