9a86a3720d3625490f96f326e11e02d0755c94e0
Ezralx <ezrateshome.alx@gmail.com>
2023-06-13 14:39:10 -0700

    task of project 0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid : returns a pointer to a 2 dimensional array of integers.
- * escription:the grid intialized to zero
+ * alloc_grid - create a 2d integer grid using malloc.
+ * Description: Each element of the grid should be initialized to 0.
  * @width: int size width
  * @height: int size height
  * Return: Pointer to new grid
