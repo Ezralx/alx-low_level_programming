f120a012843b07ef85183220533f5b8c74520ee2
Ezralx <ezrateshome.alx@gmail.com>
2023-06-21 16:15:35 -0700

    0x0F. C - Function pointers tasks

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * get_op_func - Selects the correct function to perform
- * the operation asked by the user.
+ *               the operation asked by the user.
  * @s: The operator passed as argument.
  *
  * Return: A pointer to the function corresponding
@@ -11,19 +11,19 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-			{"+", op_add},
-			{"-", op_sub},
-			{"*", op_mul},
-			{"/", op_div},
-			{"%", op_mod},
-			{NULL, NULL},
-			};
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+
 	int i = 0;
 
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
-			i++;
+		i++;
+
 	return (ops[i].f);
 }
-
-
 
