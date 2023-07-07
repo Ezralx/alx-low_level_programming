9fc9ed20ce7e8ada953a0a872ed3ae0d42f625f7
Ezralx <ezrateshome.alx@gmail.com>
2023-07-07 07:22:31 -0700

    Static Laibraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *s)
+
+{
+
+    puts(s);
+
+}
+
---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -c *.c && ar -src liball.a *.o
+
