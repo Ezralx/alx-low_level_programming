b37486db23a53d22c88a5450ca3bffb09c6b8811
Ezra Teshome <you@example.com>
2023-09-14 12:11:01 -0700

    deletes the node at index index of a dlistint_t linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,33 +1,49 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Deletes a node from a dlistint_t at a given index.
- * @head: A pointer to the head of the dlistint_t.
- * @index: The index of the node to delete.
+ * delete_dnodeint_at_index - deletes the node at index of a linked list.
+ * @head: pointer to pointer to the h of linked list.
+ * @index: ndex of the node that should be deleted. Index starts at 0.
  *
- * Return: Upon success - 1.
- *         Otherwise - -1.
- */
-#include "lists.h"
-
-/**
- * sum_dlistint - function that returns the sum of all the,
- * data (n) of a linked list.
- * @head: pointer to pointer to head of linked list.
- *
- * If the list is empty, return 0.
- *
- * Return: The sum.
+ * Return: if it succeeded, -1 if it failed
  */
 
-int sum_dlistint(dlistint_t *head)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	int sum = 0;
+	dlistint_t *head1;
+	dlistint_t *head2;
+	unsigned int count;
 
-	while (head != NULL)
+	head1 = *head;
+
+	if (head1 != NULL)
+		while (head1->prev != NULL)
+			head1 = head1->prev;
+	count = 0;
+
+	while (head1 != NULL)
 	{
-		sum += head->n;
-		head = head->next;
+		if (count == index)
+		{
+			if (count == 0)
+			{
+				*head = head1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				head2->next = head1->next;
+
+				if (head1->next != NULL)
+					head1->next->prev = head2;
+			}
+			free(head1);
+			return (1);
+		}
+		head2 = head1;
+		head1 = head1->next;
+		count++;
 	}
-	return (sum);
+	return (-1);
 }
