bbcc70f4e704361abce5bb27687f63272a8ca5bf
Ezra Teshome <you@example.com>
2023-09-14 12:04:11 -0700

    deletes the node at index index of a dlistint_t linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,34 +8,26 @@
  * Return: Upon success - 1.
  *         Otherwise - -1.
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns the sum of all the,
+ * data (n) of a linked list.
+ * @head: pointer to pointer to head of linked list.
+ *
+ * If the list is empty, return 0.
+ *
+ * Return: The sum.
+ */
+
+int sum_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp = *head;
+	int sum = 0;
 
-	if (*head == NULL)
-		return (-1);
-
-	for (; index != 0; index--)
+	while (head != NULL)
 	{
-		if (tmp == NULL)
-			return (-1);
-		tmp = tmp->next;
+		sum += head->n;
+		head = head->next;
 	}
-
-	if (tmp == *head)
-	{
-		*head = tmp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-	}
-
-	else
-	{
-		tmp->prev->next = tmp->next;
-		if (tmp->next != NULL)
-			tmp->next->prev = tmp->prev;
-	}
-
-	free(tmp);
-	return (1);
+	return (sum);
 }
