81b1da6a27494440878068fa563d2bb26f702c2d
Ezralx <ezrateshome.alx@gmail.com>
2024-01-25 17:08:16 +0300

    Update(task 2: check if address of head pointer is given

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,11 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+/*	check if address of 
+	head pointer is given
+*/
+	if(head == NULL)
+	  return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
