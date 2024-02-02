be56a9eb7ec2a0a2ba19761031fa811e307afe01
Ezralx <ezrateshome.alx@gmail.com>
2024-02-02 21:29:09 +0300

    fix: adjust compilation of shared library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Ezralx/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libezra.so
-export LD_PRELOAD='/tmp/libezra.so'
+wget -P /tmp/ https://github.com/Ezralx/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libezra_rand.so
+export LD_PRELOAD='/tmp/libezra_rand.so'
