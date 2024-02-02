8340d07945f2750fc6dc1a13f4e5efe3b16ee839
Ezralx <ezrateshome.alx@gmail.com>
2024-02-02 21:39:50 +0300

    chore: adjust link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Ezralx/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libezra_rand.so
+wget -P /tmp/ https://github.com/Ezralx/alx-low_level_programming/blob/master/0x18-dynamic_libraries/giga_millions/libezra_rand.so
 export LD_PRELOAD='/tmp/libezra_rand.so'
