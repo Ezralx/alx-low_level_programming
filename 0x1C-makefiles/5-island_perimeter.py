533c02cd1ac2039416b1a63a764c7e63d2ab6350
Ezralx <ezrateshome.alx@gmail.com>
2024-04-05 00:29:44 +0300

    feat: Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module that contains the island_perimeter function """
+
+
+def island_perimeter(grid):
+    """ Calculate perimeter of grid where "1" is found """
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1  # top
+                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
+                    perimeter += 1  # bottom
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1  # left
+                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
+                    perimeter += 1  # right
+
+    return perimeter
